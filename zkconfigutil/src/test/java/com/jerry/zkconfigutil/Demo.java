b9ad1578be391a97a467f0e8e998f0324dafe1f8
jerry <search.jerry@gmail.com>
2014-02-18 11:11:23 +0800

    nothing

--- zkconfigutil/src/test/java/com/jerry/zkconfigutil/Demo.java
+++ zkconfigutil/src/test/java/com/jerry/zkconfigutil/Demo.java
@@ -14,7 +14,7 @@ import com.jerry.zkconfigutil.annotation.TypeZkConfigurable;
 @TypeZkConfigurable(path="/test/jerry", server="localhost:2181", useOwnZkServer=false)
 public final class Demo {
 	
-	@FieldZkConfigurable(resove=DemoF1Resolve.class, dynamicUpdate=false)
+	@FieldZkConfigurable(resove=DemoF1Resolve.class, dynamicUpdate=true)
 	public static String F1 = "F1";
 	
 	@FieldZkConfigurable(resove=DemoF2Resolve.class, dynamicUpdate=true)
