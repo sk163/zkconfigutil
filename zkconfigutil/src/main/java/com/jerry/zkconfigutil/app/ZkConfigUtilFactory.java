87eba145baea668b68fbb945e6ca76e5721ecddc
jerry <search.jerry@gmail.com>
2014-04-02 11:18:58 +0800

    add synchronized

--- zkconfigutil/src/main/java/com/jerry/zkconfigutil/app/ZkConfigUtilFactory.java
+++ zkconfigutil/src/main/java/com/jerry/zkconfigutil/app/ZkConfigUtilFactory.java
@@ -4,13 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public final class ZkConfigUtilFactory {
-	
+
 	private static final Map<String, ZkConfigUtil> cache = new HashMap<String, ZkConfigUtil>();
-	
-	public static ZkConfigUtil getZkConfigUtil(String servers){
-		if(cache.containsKey(servers)){
+
+	public static synchronized ZkConfigUtil getZkConfigUtil(String servers) {
+		if (cache.containsKey(servers)) {
 			return cache.get(servers);
-		}else{
+		} else {
 			ZkConfigUtil zkConfigUtil = new ZkConfigUtil(servers);
 			cache.put(servers, zkConfigUtil);
 			return zkConfigUtil;
